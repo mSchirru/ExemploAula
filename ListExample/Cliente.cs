241d0dac11d564f977b32e2ecdb404984cb43ce6
mSchirru <2014.schirru@gmail.com>
2017-11-12 23:50:32 -0200

    Initial check-in of module ListExample

---
+++ ListExample/Cliente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListExample
+{
+    public class Cliente
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string cpf;
+        public string Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Nome:{0}\n Cpf:{1}\n Age:{2}\n", Name, Cpf, Age);
+        }
+
+        List<Cliente> listaCliente = new List<Cliente>();
+
+
+        public Cliente Add()
+        {
+            Cliente cli = new Cliente();
+            Console.Write("Digite o nome do cliente: ");
+            cli.Name = Console.ReadLine();
+            Console.Write("Digite o cpf do cliente: ");
+            cli.Cpf = Console.ReadLine();
+            Console.Write("Digite a idade do cliente: ");
+            cli.Age = int.Parse(Console.ReadLine());
+
+            listaCliente.Add(cli);
+
+            Console.WriteLine("Detalhes do cliente - {0}\n", cli);
+
+            return cli;
+        }
+
+        public void showList()
+        {
+			Console.WriteLine("Lista de clientes: ");
+
+			foreach (Cliente c in listaCliente)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+
+        public void Remove()
+        {
+            Console.Write("Digite o cpf do cliente a ser removido:");
+            string cpfremove = Console.ReadLine();
+			var itemToRemove = listaCliente.SingleOrDefault(r => r.Cpf == cpfremove);
+			if (itemToRemove != null)
+				listaCliente.Remove(itemToRemove);
+
+        }
+
+    }
+}
---
+++ ListExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ListExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Cliente c1 = new Cliente();
+            int menuchoice = 0;
+
+            while (menuchoice != 4)
+            {
+
+                Console.WriteLine("MENU");
+                Console.WriteLine("Escolha uma das opcoes abaixo: \n");
+
+                Console.WriteLine("1. Add");
+
+                Console.WriteLine("2. Show list");
+
+                Console.WriteLine("3. Remove");
+
+                Console.WriteLine("4. Exit \n");
+
+
+
+                menuchoice = int.Parse(Console.ReadLine());
+
+                switch (menuchoice)
+                {
+                    case 1:
+                        Console.WriteLine("*********** add ***********");
+                        c1.Add();
+                        Console.Clear();
+
+                        break;
+                    case 2:
+                        Console.WriteLine("*********** SHOW ***********");
+                        c1.showList();
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+					case 3:
+						Console.WriteLine("*********** remove ***********");
+						c1.Remove();
+                        Console.Clear();
+						break;
+
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Digite uma opcao valida!");
+                        break;
+                }
+            }
+
+        }
+    }
+}
